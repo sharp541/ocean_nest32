522f579aabb0d75aeb36edc8a3542b4a780828ee
sharp541 <stonegate541@gmail.com>
2025-10-03 20:50:17 +0900

    feat: add mouse button functionality to keymap for enhanced control

--- qmk_firmware/ocean_nest32/keymaps/default/keymap.c
+++ qmk_firmware/ocean_nest32/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_F6  ,    KC_F7  ,    KC_F8  ,    KC_F9  ,                                        QK_BOOT,    XXXXXXX,    XXXXXXX,    XXXXXXX,
         KC_F10 ,    KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,    KC_F5  ,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
                                 XXXXXXX,    XXXXXXX,    KC_F12 ,    KC_F11 ,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-                                                        XXXXXXX,    XXXXXXX,                XXXXXXX,    XXXXXXX
+                                                        XXXXXXX,    MS_BTN2,                XXXXXXX,    XXXXXXX
     )
 };
 
