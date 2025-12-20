e89670c1ff7b2b1fa1c03570bbed868b83860c49
sharp541 <stonegate541@gmail.com>
2025-12-20 16:36:26 +0900

    fix: update PID in keyboard.json and refine key assignments in default keymap for improved functionality

--- qmk_firmware/ocean_nest32/keymaps/default/keymap.c
+++ qmk_firmware/ocean_nest32/keymaps/default/keymap.c
@@ -18,42 +18,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ALPHA1_LAYER] = LAYOUT(
         KC_P,       KC_K,       KC_R,       KC_Z,                                                       KC_C,       KC_U,       KC_E,       KC_MINS,
         KC_M,       KC_S,       KC_N,       KC_T,       KC_G,                               KC_DOT,     KC_I,       KC_A,       KC_O,       KC_D,
-        KC_H,                               KC_Y,                                                       KC_COMM,    MS_BTN1,    LB_MO ,     KC_W,
+                                            KC_Y,                                                       KC_COMM,    MS_BTN1,    LB_MO ,
                                                         SPC_A2 ,    TAB_NUM ,   DEL_FUN,    BSP_SYM
     ),
 
     [_ALPHA2_LAYER] = LAYOUT(
         _______,    KC_V,       _______,    _______,                                                    CKC_BRCT,   KC_TILD,    _______,    _______,
         KC_X,       KC_B,       KC_L,       KC_F,       KC_Q,                               _______,    CKC_PRN,    CKC_BRC,    _______,    _______,
-        _______,                            KC_J,                                                       KC_LT,      KC_GT,      _______ ,   _______,
+                                            KC_J,                                                       KC_LT,      KC_GT,      _______ ,
                                                         _______,    MS_BTN2 ,   _______,    CW_TOGG
     ),
 
     [_MOUSE_LAYER] = LAYOUT(
         _______,    C(KC_S),    C(KC_Z),    C(KC_Y),                                                    _______,    _______,    _______,    _______,
         C(KC_X),    C(KC_C),    C(KC_V),    C(KC_SLSH), _______,                            C(KC_PGUP), C(KC_PGDN), KC_BTN3,    _______,    _______,
-        C(KC_A),                            SGUI(KC_S),                                                 C(KC_T),    C(KC_W),    _______,    RCS(KC_T),
+                                            SGUI(KC_S),                                                 C(KC_T),    C(KC_W),    _______,
                                                         C(KC_LBRC), C(KC_RBRC), KC_WFWD,    KC_WBAK
     ),
 
     [_SYMBOL_LAYER] = LAYOUT(
         KC_CIRC,    KC_PERC,    KC_ASTR,    KC_PLUS,                                                    KC_DQT,     KC_QUOT,    KC_GRV,     _______,
         KC_AT,      KC_HASH,    KC_DLR,     KC_AMPR ,   KC_PIPE,                            KC_UNDS,    KC_EQL,     _______,    KC_COLN,    KC_SCLN,
-        KC_EXLM,                            KC_QUES,                                                    CKC_SL ,    _______ ,   _______,    _______,
+                                            KC_QUES,                                                    CKC_SL ,    _______ ,   _______,
                                                         KC_LNG2,    KC_LNG1,    _______,    _______
     ),
 
     [_NUMPAD_LAYER] = LAYOUT(
         KC_6   ,    KC_7   ,    KC_8   ,    KC_9   ,                                                    _______,    _______,    _______,    _______,
         KC_1   ,    KC_2   ,    KC_3   ,    KC_4   ,    KC_5   ,                            _______,    _______,    _______,    _______,    _______,
-        KC_0,                               KC_DOT ,                                                    _______,    _______,    _______,    _______,
+                                            KC_0,                                                       _______,    _______,    _______,
                                                         _______,    _______,    _______,    _______
     ),
 
     [_FUNCTION_LAYER] = LAYOUT(
         KC_F6  ,    KC_F7  ,    KC_F8  ,    KC_F9  ,                                                    QK_BOOT,    _______,    _______,    _______,
         KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,    KC_F5  ,                            _______,    _______,    _______,    _______,    _______,
-        KC_F10,                             KC_F11 ,                                                    _______,    _______,    _______,    _______,
+                                            KC_F10 ,                                                    _______,    _______,    _______,
                                                         _______,    _______,    _______,    _______
     )
 };
