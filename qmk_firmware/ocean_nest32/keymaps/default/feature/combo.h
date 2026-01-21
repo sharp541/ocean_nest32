a3e46c14f6f1d2105cca0171e47159391e474b54
sharp541 <stonegate541@gmail.com>
2026-01-21 16:16:21 +0900

    fix: update combo definitions and key assignments for improved functionality

--- qmk_firmware/ocean_nest32/keymaps/default/feature/combo.h
+++ qmk_firmware/ocean_nest32/keymaps/default/feature/combo.h
@@ -11,40 +11,56 @@ enum combos {
     UARR_L_COMBO,
     DARR_R_COMBO,
     DARR_L_COMBO,
+    RARR_R_COMBO,
+    RARR_L_COMBO,
+    LARR_R_COMBO,
+    LARR_L_COMBO,
     HOME_R_COMBO,
     END_R_COMBO,
     ESC_R_COMBO,
     ALT_L_COMBO,
-    GUI_L_COMBO
+    GUI_L_COMBO,
+    CW_TOGG_COMBO
 };
 
 // enter
-const uint16_t PROGMEM ent_r_combo[] = {KC_A, KC_O, COMBO_END};
+const uint16_t PROGMEM ent_r_combo[] = {KC_T, KC_O, COMBO_END};
 const uint16_t PROGMEM ent_l_combo[] = {KC_S, KC_N, COMBO_END};
 
 // backspace
-const uint16_t PROGMEM bspc_r_combo[] = {KC_I, KC_A, KC_O, COMBO_END};
-const uint16_t PROGMEM bspc_l_combo[] = {KC_S, KC_N, KC_T, COMBO_END};
+const uint16_t PROGMEM bspc_r_combo[] = {KC_A, KC_T, KC_O, COMBO_END};
+const uint16_t PROGMEM bspc_l_combo[] = {KC_S, KC_N, KC_I, COMBO_END};
 
 // up arrow
-const uint16_t PROGMEM uarr_r_combo[] = {KC_U, KC_E, COMBO_END};
+const uint16_t PROGMEM uarr_r_combo[] = {KC_E, KC_M, COMBO_END};
 const uint16_t PROGMEM uarr_l_combo[] = {KC_K, KC_R, COMBO_END};
 
 // down arrow
-const uint16_t PROGMEM darr_r_combo[] = {KC_I, KC_U, KC_E, COMBO_END};
-const uint16_t PROGMEM darr_l_combo[] = {KC_K, KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM darr_r_combo[] = {KC_A, KC_E, KC_M, COMBO_END};
+const uint16_t PROGMEM darr_l_combo[] = {KC_K, KC_R, KC_I, COMBO_END};
+
+// right arrow
+const uint16_t PROGMEM rarr_r_combo[] = {KC_A, KC_M, COMBO_END};
+const uint16_t PROGMEM rarr_l_combo[] = {KC_R, KC_I, COMBO_END};
+
+// left arrow
+const uint16_t PROGMEM larr_r_combo[] = {KC_A, KC_E, COMBO_END};
+const uint16_t PROGMEM larr_l_combo[] = {KC_K, KC_I, COMBO_END};
 
 // home
-const uint16_t PROGMEM home_r_combo[] = {KC_E, KC_MINS, COMBO_END};
+const uint16_t PROGMEM home_r_combo[] = {KC_M, KC_D, COMBO_END};
 
 // end
-const uint16_t PROGMEM end_r_combo[] = {KC_A, KC_E, KC_MINS, COMBO_END};
+const uint16_t PROGMEM end_r_combo[] = {KC_M, KC_D, KC_O, COMBO_END};
 
 // esc
-const uint16_t PROGMEM esc_r_combo[] = {KC_DOT, KC_C, COMBO_END};
+const uint16_t PROGMEM esc_r_combo[] = {KC_F, KC_Z, COMBO_END};
 
 // alt
-const uint16_t PROGMEM alt_l_combo[] = {KC_Z, KC_G, COMBO_END};
+const uint16_t PROGMEM alt_l_combo[] = {KC_W, KC_G, COMBO_END};
 
 // gui
-const uint16_t PROGMEM gui_l_combo[] = {KC_M, KC_S, COMBO_END};
+const uint16_t PROGMEM gui_l_combo[] = {KC_B, KC_S, COMBO_END};
+
+// caps word
+const uint16_t PROGMEM cw_togg_combo[] = {KC_I, KC_A, COMBO_END};
--- qmk_firmware/ocean_nest32/keymaps/default/keymap.c
+++ qmk_firmware/ocean_nest32/keymaps/default/keymap.c
@@ -16,55 +16,55 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ALPHA1_LAYER] = LAYOUT(
-        KC_P,       KC_K,       KC_R,       KC_Z,                                                       KC_C,       KC_U,       KC_E,       KC_MINS,
-        KC_M,       KC_S,       KC_N,       KC_T,       KC_G,                               KC_DOT,     KC_I,       KC_A,       KC_O,       KC_D,
-                                            KC_Y,                                                       KC_COMM,    MS_BTN1,    LB_MO ,
+        KC_H,       KC_K,       KC_R,       KC_W,                                                       KC_F,       KC_E,       KC_M,       KC_D,
+        KC_B,       KC_S,       KC_N,       KC_I,       KC_G,                               KC_Z,       KC_A,       KC_O,       KC_T,       KC_P,
+                                            KC_Y,                                                       KC_U,       MS_BTN1,    LB_MO ,
                                                         SPC_A2 ,    TAB_NUM ,   DEL_FUN,    BSP_SYM
     ),
 
     [_ALPHA2_LAYER] = LAYOUT(
-        _______,    KC_V,       _______,    _______,                                                    CKC_BRCT,   KC_TILD,    _______,    _______,
-        KC_X,       KC_B,       KC_L,       KC_F,       KC_Q,                               _______,    CKC_PRN,    CKC_BRC,    _______,    _______,
-                                            KC_J,                                                       KC_LT,      KC_GT,      _______ ,
-                                                        _______,    MS_BTN2 ,   _______,    CW_TOGG
+        _______,    KC_V,       KC_X,       _______,                                                    KC_LBRC,    KC_RBRC,    KC_LT,      KC_GT,
+        _______,    KC_J,       KC_L,       KC_C,       _______,                            _______,    KC_LPRN,    KC_RPRN,    KC_LCBR,    KC_RCBR,
+                                            KC_Q,                                                       _______,    _______,    _______ ,
+                                                        _______,    MS_BTN2 ,   KC_COMM,    KC_DOT
     ),
 
     [_MOUSE_LAYER] = LAYOUT(
-        _______,    C(KC_S),    C(KC_Z),    C(KC_Y),                                                    _______,    _______,    _______,    _______,
-        C(KC_X),    C(KC_C),    C(KC_V),    C(KC_SLSH), _______,                            C(KC_PGUP), C(KC_PGDN), KC_BTN3,    _______,    _______,
+        C(KC_A),    C(KC_S),    C(KC_Z),    C(KC_Y),                                                    _______,    _______,    _______,    _______,
+        C(KC_X),    C(KC_C),    C(KC_V),    C(KC_SLSH), _______,                            _______,    RCS(KC_T),  KC_BTN3,    _______,    _______,
                                             SGUI(KC_S),                                                 C(KC_T),    C(KC_W),    _______,
                                                         C(KC_LBRC), C(KC_RBRC), KC_WFWD,    KC_WBAK
     ),
 
     [_SYMBOL_LAYER] = LAYOUT(
-        KC_CIRC,    KC_PERC,    KC_ASTR,    KC_PLUS,                                                    KC_DQT,     KC_QUOT,    KC_GRV,     _______,
-        KC_AT,      KC_HASH,    KC_DLR,     KC_AMPR ,   KC_PIPE,                            KC_UNDS,    KC_EQL,     _______,    KC_COLN,    KC_SCLN,
-                                            KC_QUES,                                                    CKC_SL ,    _______ ,   _______,
+        KC_CIRC,    KC_PERC,    KC_ASTR,    KC_PLUS,                                                    KC_DQT,     KC_QUOT,    KC_GRV,     KC_TILD,
+        KC_AT,      KC_HASH,    KC_DLR,     KC_AMPR ,   KC_PIPE,                            KC_UNDS,    KC_MINS,    KC_EQL,     KC_COLN,    KC_SCLN,
+                                            KC_QUES,                                                    KC_SLSH ,   KC_BSLS ,   KC_EXLM,
                                                         KC_LNG2,    KC_LNG1,    _______,    _______
     ),
 
     [_NUMPAD_LAYER] = LAYOUT(
-        KC_6   ,    KC_7   ,    KC_8   ,    KC_9   ,                                                    _______,    _______,    _______,    _______,
-        KC_1   ,    KC_2   ,    KC_3   ,    KC_4   ,    KC_5   ,                            _______,    _______,    _______,    _______,    _______,
-                                            KC_0,                                                       _______,    _______,    _______,
+        KC_5   ,    KC_6   ,    KC_7   ,    KC_8   ,                                                    _______,    _______,    _______,    _______,
+        KC_0   ,    KC_1   ,    KC_2   ,    KC_3   ,    KC_4   ,                            _______,    _______,    _______,    _______,    _______,
+                                            KC_9,                                                       _______,    _______,    _______,
                                                         _______,    _______,    _______,    _______
     ),
 
     [_FUNCTION_LAYER] = LAYOUT(
-        KC_F6  ,    KC_F7  ,    KC_F8  ,    KC_F9  ,                                                    QK_BOOT,    _______,    _______,    _______,
-        KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,    KC_F5  ,                            _______,    _______,    _______,    _______,    _______,
-                                            KC_F10 ,                                                    _______,    _______,    _______,
+        KC_F5  ,    KC_F6  ,    KC_F7  ,    KC_F8  ,                                                    QK_BOOT,    _______,    _______,    _______,
+        KC_F10 ,    KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,                            _______,    _______,    _______,    _______,    _______,
+                                            KC_F9 ,                                                    _______,    _______,    _______,
                                                         _______,    _______,    _______,    _______
     )
 };
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_ALPHA1_LAYER] =   { ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+    [_ALPHA1_LAYER] =   { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
     [_ALPHA2_LAYER] =   { ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
     [_MOUSE_LAYER]  =   { ENCODER_CCW_CW(MS_WHLR, MS_WHLL) },
-    [_SYMBOL_LAYER] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_SYMBOL_LAYER] =   { ENCODER_CCW_CW(C(KC_PGUP), C(KC_PGDN)) },
     [_NUMPAD_LAYER] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_FUNCTION_LAYER]=  { ENCODER_CCW_CW(MS_WHLR, MS_WHLL) },
+    [_FUNCTION_LAYER]=  { ENCODER_CCW_CW(LSA(KC_TAB), A(KC_TAB)) }
 };
 
 
@@ -77,9 +77,14 @@ combo_t key_combos[] = {
     [UARR_L_COMBO] = COMBO(uarr_l_combo, KC_UP),
     [DARR_R_COMBO] = COMBO(darr_r_combo, KC_DOWN),
     [DARR_L_COMBO] = COMBO(darr_l_combo, KC_DOWN),
+    [RARR_R_COMBO] = COMBO(rarr_r_combo, KC_RIGHT),
+    [RARR_L_COMBO] = COMBO(rarr_l_combo, KC_RIGHT),
+    [LARR_R_COMBO] = COMBO(larr_r_combo, KC_LEFT),
+    [LARR_L_COMBO] = COMBO(larr_l_combo, KC_LEFT),
     [HOME_R_COMBO] = COMBO(home_r_combo, KC_HOME),
     [END_R_COMBO] = COMBO(end_r_combo, KC_END),
     [ESC_R_COMBO] = COMBO(esc_r_combo, KC_ESC),
     [ALT_L_COMBO] = COMBO(alt_l_combo, KC_LALT),
-    [GUI_L_COMBO] = COMBO(gui_l_combo, KC_LGUI)
+    [GUI_L_COMBO] = COMBO(gui_l_combo, KC_LGUI),
+    [CW_TOGG_COMBO] = COMBO(cw_togg_combo, CW_TOGG)
 };
