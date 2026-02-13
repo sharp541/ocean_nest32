8f85c02db823069493f54d9eb7ae82de878cd8f8
sharp541 <stonegate541@gmail.com>
2026-02-13 14:23:54 +0900

    feat: implement PMW3610 pointing device driver and update configuration

--- qmk_firmware/ocean_nest32/keymaps/default/rules.mk
+++ qmk_firmware/ocean_nest32/keymaps/default/rules.mk
@@ -3,7 +3,7 @@ SERIAL_DRIVER = vendor
 
 ENCODER_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
+POINTING_DEVICE_DRIVER = custom
 
 BOARD = GENERIC_RP_RP2040
 SPI_DRIVER_REQUIRED = yes
@@ -16,4 +16,10 @@ MOUSEKEY_ENABLE = yes
 COMBO_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 
-SRC += feature/layer.c feature/mouse_control.c feature/process_record.c feature/autoshift.c feature/custom_key.c
+SRC += feature/layer.c
+SRC += feature/mouse_control.c
+SRC += feature/process_record.c
+SRC += feature/autoshift.c
+SRC += feature/custom_key.c
+SRC += ../lib/pmw3610/pmw3610.c
+SRC += ../lib/pmw3610/custom_pointing_device.c
